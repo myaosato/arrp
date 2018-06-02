948692dd0e4933f6594657c5873fd3b22bf5dcdc
myaosato <tetu60u@yahoo.co.jp>
2018-06-02 11:02:54 +0900

    fix

--- src/ArrpSexpStack.js
+++ src/ArrpSexpStack.js
@@ -1,17 +1,12 @@
 'use strict';
 
 class ArrpSexpStack {
-  constructor(sexps, pos) {
+  constructor(sexps) {
     this.stack = sexps;
-    this.__num = pos + 1;
   }
 
   dequeue() {
     return this.stack.shift();
-  }
-
-  getNum() {
-    return this.__num;
   }
 }
 
